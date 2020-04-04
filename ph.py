39a1aa353d3bbb308769eb1bda1ddda2c7e4b6a6
cnails <andreyandreevgr@gmail.com>
2020-04-04 21:06:20 +0300

    a

--- ph.py
+++ ph.py
@@ -1,58 +1,55 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import numpy as np
 import cv2
 
-alpha = 0.4
+def open_and_resize_image(input, max_height, max_width):
+    img = Image.open(input)
+    if img.height > max_height and img.height > img.width:
+        # vertical image
+        percentage_decrease = (max_height * 100) / img.height / 100
+        new_height = round(img.height * percentage_decrease)
+        new_width = round(img.width * percentage_decrease)
+        img = img.resize((new_width, new_height))
 
-img2 = cv2.imread('__car_1.jpg')
-img1 = cv2.imread('car_2.jpg')
+    if (img.width > max_width and img.width > img.height) or (img.width == img.height and img.width > max_width):
+        # Horizontal image
+        percentage_decrease = (max_width * 100) / img.width / 100
+        new_height = round(img.height * percentage_decrease)
+        new_width = round(img.width * percentage_decrease)
+        img = img.resize((new_width, new_height))
+    img.save('new_size.jpg', 'JPEG')
+    return img
 
-r,c,z = img1.shape
+def bright(source_name, result_name, brightness):
+    source = Image.open(source_name)
+    result = Image.new('RGB', source.size)
+    for x in range(source.size[0]):
+        for y in range(source.size[1]):
+            r, g, b = source.getpixel((x, y))
 
-out_img = np.zeros(img1.shape,dtype=img1.dtype)
-out_img[:,:,0] = (alpha * img1[:,:,0]) + ((1-alpha) * img2[:,:,0])
-out_img[:,:,1] = (alpha * img1[:,:,1]) + ((1-alpha) * img2[:,:,1])
-out_img[:,:,2] = (alpha * img1[:,:,2]) + ((1-alpha) * img2[:,:,2])
-'''
-# if want to loop over the whole image
-for y in range(r):
-    for x in range(c):
-        out_img[y,x,0] = (alpha * img1[y,x,0]) + ((1-alpha) * img2[y,x,0])
-        out_img[y,x,1] = (alpha * img1[y,x,1]) + ((1-alpha) * img2[y,x,1])
-        out_img[y,x,2] = (alpha * img1[y,x,2]) + ((1-alpha) * img2[y,x,2])
-'''
+            red = int(r * brightness)
+            red = min(255, max(0, red))
 
-# im1 = Image.open("__car_1.jpg")
-# im2 = Image.open("car_2.jpg")
-# blended = Image.blend(im1, im2, alpha=0.5)
-# blended.save("blended.png")
+            green = int(g * brightness)
+            green = min(255, max(0, green))
 
-# from PIL import Image
-# import glob
+            blue = int(b * brightness)
+            blue = min(255, max(0, blue))
 
-# foreground_name = 'car_2.jpg'
-# foreground = Image.open(foreground_name)
-# foreground = foreground.convert('RGBA')
+            result.putpixel((x, y), (red, green, blue))
+    result.save(result_name, "JPEG")
 
-# for file_name in glob.glob('*.jpg'):
-#     print (file_name)
-#     background_name = file_name
-#     background = Image.open(background_name)
-
-#     x = int((background.size[0] / 2) - (foreground.size[0] / 2))
-#     y = int((background.size[1] / 2) - (foreground.size[1] / 2))
-
-#     background = background.convert('RGBA')
-#     background.paste(foreground, (x, y), mask = foreground)
-#     background = background.convert('RGB')
-#     background.save('__' + background_name.split('.')[0] + '.jpg','JPEG')
-
-# print('Gotovo bla-a')
-
-# out_img = img2.copy()
-
-# out_img = cv2.add(img1, img2)
-# cv2.addWeighted(out_img, 0.5, img1, 0.5, img2)
-
-# cv2.imshow('Output',out_img)
-# cv2.waitKey(0)
+if __name__ == "__main__":
+    im1 = Image.open('car_1.jpg')
+    im1 = im1.convert('RGBA')
+    im2 = Image.open('car_2.jpg')
+    im2 = im2.convert('RGBA')
+    im2 = im2.resize(im1.size)
+    # print(im2.split())
+    alpha = ImageEnhance.Brightness(im2.split()[3]).enhance(0.30)
+    im2.putalpha(alpha)
+    # img2 = cv2.imread('car_2.jpg')
+    # height, width = img2.shape[:2]
+    # print(img2.shape[:2])
+    # open_and_resize_image('car_1.jpg', height, width)
+    Image.alpha_composite(im1, im2).save("test3.png")
