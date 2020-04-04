bfb74edc64e53fdd419c7b7dcca7a7d27af6bc1e
cnails <andreyandreevgr@gmail.com>
2020-04-05 00:03:17 +0300

    a

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -79,6 +79,19 @@ from flask_wtf.file import FileField, FileAllowed
 # 	logout_user()
 # 	return redirect(url_for('register'))
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 class Picture:
 	picture = FileField('Загрузить фото', validators=[FileAllowed(['jpg', 'png'])])
 
