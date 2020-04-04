63a2722c436358aef605c223b475ff37b5848050
Eduard <happypuffin7@gmail.com>
2020-04-04 23:20:19 +0000

    bugfix

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -100,7 +100,7 @@ def save_picture(form_picture):
 	_, f_ext = os.path.splitext(form_picture.filename)
 	picture_fn = random_hex + f_ext
 	picture_path = os.path.join(app.root_path, 'static/profile_pic', picture_fn)
-    process_picture(picture_path)
+	process_picture(picture_path)
 	i = Image.open(form_picture)
 	# i.thumbnail(output_size)
 
