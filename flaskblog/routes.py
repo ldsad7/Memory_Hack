8e7332488a977123c8ed9659ea98693c1949cccd
cnails <andreyandreevgr@gmail.com>
2020-04-05 03:29:21 +0300

    a

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -111,16 +111,10 @@ def save_picture(form_picture):
 def editor():
 	print('here')
 	if request.method == 'POST':
-		# check if the post request has the file part
-		# if 'file' not in request.files:
-			# flash('No file part')
-			# return redirect(request.url)
+		print('hello')
 		file = request.files['file']
-		# if user does not select file, browser also
-		# submit an empty part without filename
 		if file.filename == '':
 			flash('No selected file')
-			# return redirect(request.url)
 		if file:
 			filename = file.filename
 			file.save(filename)
