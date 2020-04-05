5a092d2becfc75e6ee7df208d687d68229ef6af1
Erik <erikuz1997@gmail.com>
2020-04-05 03:25:48 +0300

    Добавил ajax запрос

--- flaskblog/static/scripts/main.js
+++ flaskblog/static/scripts/main.js
@@ -1,9 +1,21 @@
 $(function() {
-    // $('.panels').slideToggle();
-    // $('.panels').show();
-    $('button.bottom_music').click(function () {
-        $('.panels').slideToggle();
+    let files;
+    let fdata = new FormData();
+$("inpit[type=file]").on("change", function (e) {
+    files = this.files;
+
+    $.each(files, function (i, file) {
+        fdata.append("file" + i, file);
     });
+
+    $.ajax({
+        url: "/upload_photo",
+        type: "post",
+        data: fdata, 
+        processData: false, 
+        contentType: false, 
+    });
+});
 
 
 });
