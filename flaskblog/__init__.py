3f0999ed3094c18525e4156f86b689bc7af8f050
cnails <andreyandreevgr@gmail.com>
2020-04-05 14:53:13 +0300

    deleted thrash

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,10 +11,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.config['SECRET_KEY'] = '203ad5ffa1d7c650ad681fdff3965cd2'
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+# db = SQLAlchemy(app)
+# bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -8,9 +8,9 @@ import numpy as np
 
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort, send_file, make_response
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from flaskblog.models import User, Post
+from flaskblog import app#, db, bcrypt
+# from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+# from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_wtf.file import FileField, FileAllowed
 from PIL import ImageEnhance, Image
